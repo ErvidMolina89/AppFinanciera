f9c46abea53c5c575c6be415e0299a340161f8fe
ErvidMolina89 <eralmoba@hotmail.com>
2024-05-30 10:32:36 -0500

    Manejo de warnings y eliminación de import basuras

--- app/src/main/java/com/wposs/appfinanciera/View/MovementDetail/MovementDetailActivity.java
+++ app/src/main/java/com/wposs/appfinanciera/View/MovementDetail/MovementDetailActivity.java
@@ -3,7 +3,6 @@ package com.wposs.appfinanciera.View.MovementDetail;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import com.wposs.appfinanciera.Base.App;
